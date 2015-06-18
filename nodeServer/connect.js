6a52eb9533aeac60c1d32be681e879d1caa4c2a9
YCS-Aron <ycs@tradeshift.com>
2015-06-19 00:06:25 +0800

    new template

--- nodeServer/connect.js
+++
@@ -1,7 +0,0 @@
-var connect = require('connect'),
-    serveStatic = require('serve-static');
-
-var app = connect();
-app.use(serveStatic("../../mywebsite"));
-app.listen(3000);
-console.log('started server,listening at port 80');
